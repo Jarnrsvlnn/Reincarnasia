05493c46b7246283f1d8fda63da783a35c5cf13a
Jarnrsvlnn <sevillenaeduc@gmail.com>
2025-01-16 09:36:51 +0800

    implemented sprites for main character

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
-import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.awt.Graphics2D;
 
 public class Player extends Entity {
 
@@ -14,31 +17,63 @@ public class Player extends Entity {
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
+        getPlayerImage();
     }
+
+    public void getPlayerImage () {
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/resource/BaseUp.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/resource/BaseDown.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/resource/BaseLeft.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/resource/BaseRight.png"));
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
 
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
 
     public void update() {
         if(keyH.upPressed == true){
+            direction = "up";
             y -= speed;
         }
         else if(keyH.downPressed == true){
+            direction = "down";
             y += speed;
         }
         else if(keyH.leftPressed == true){
+            direction = "left";
             x -= speed;
         }
         else if(keyH.rightPressed == true){
+            direction = "right";
             x += speed;
         }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+
+        switch(direction){
+            case "up":
+                image = up1;
+                break;
+            case "down":
+                image = down1;
+                break;
+            case "left":
+                image = left1;
+                break;
+            case "right":
+                image = right1;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
